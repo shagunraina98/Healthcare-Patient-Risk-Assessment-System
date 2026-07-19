386aea906c85bb02906614475fbaa623b1a02d86
Shagun raina <shagun.raina1@gmail.com>
2026-07-19 16:05:10 +0530

    Added input validation for patient data

--- Healthcare PatientRiskAssessmentSystem.py
+++ Healthcare PatientRiskAssessmentSystem.py
@@ -7,7 +7,11 @@ oxygen = int(input("Enter Oxygen Level (SpO2): "))
 print("\n----- Patient Report -----")
 print("Patient Name:", name)
 
-if oxygen < 90 or temperature > 40:
+if oxygen < 0 or oxygen > 100:
+    print("Invalid Oxygen Level")
+elif temperature < 30 or temperature > 45:
+    print("Invalid Temperature")
+elif oxygen < 90 or temperature > 40:
     print("Risk Level: Critical")
     print("Recommendation: Immediately transfer the patient to ICU.")
 elif (90 <= oxygen <= 94) or (38 <= temperature <= 40):
