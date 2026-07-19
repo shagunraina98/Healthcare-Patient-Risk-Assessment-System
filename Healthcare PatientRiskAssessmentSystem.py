1f2d44c1c452d7ef697e555943017e0d34d7a472
Shagun raina <shagun.raina1@gmail.com>
2026-07-19 16:00:18 +0530

    Initial implementation of Healthcare Patient Risk Assessment System

---
+++ Healthcare PatientRiskAssessmentSystem.py
@@ -0,0 +1,21 @@
+name = input("Enter Patient Name: ")
+age = int(input("Enter Age: "))
+temperature = float(input("Enter Body Temperature (°C): "))
+heart_rate = int(input("Enter Heart Rate (BPM): "))
+oxygen = int(input("Enter Oxygen Level (SpO2): "))
+
+print("\n----- Patient Report -----")
+print("Patient Name:", name)
+
+if oxygen < 90 or temperature > 40:
+    print("Risk Level: Critical")
+    print("Recommendation: Immediately transfer the patient to ICU.")
+elif (90 <= oxygen <= 94) or (38 <= temperature <= 40):
+    print("Risk Level: High")
+    print("Recommendation: Doctor consultation and hospital admission required.")
+elif heart_rate > 100 or age > 60:
+    print("Risk Level: Medium")
+    print("Recommendation: Keep the patient under observation.")
+else:
+    print("Risk Level: Low")
+    print("Recommendation: Patient is healthy and can go home after consultation.")
